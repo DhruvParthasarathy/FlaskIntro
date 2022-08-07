132ad074dc2633f79006e5d57f3f44ae6dcb1d67
DhruvParthasarathy <parthdhruv@gmail.com>
2022-08-08 01:10:22 +0530

    Added templates and adding db

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
